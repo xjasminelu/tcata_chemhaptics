19c234d5754101f93c897775db498200d45b870f
Jasmine Lu <jasminelu@Jasmines-MacBook-Air.local>
2020-12-14 19:36:05 -0600

    init page

--- tcata.js
+++ tcata.js
@@ -2,6 +2,15 @@ let sessionOutput;
 var tcataState;
 let attributes;
 
+var state = 'INIT';
+var canvas;
+
+//init_view
+var init_div;
+var name;
+var chemID;
+
+
 function preload() {
 	attributes = loadStrings('Attributes.txt');
 }
@@ -9,12 +18,11 @@ function preload() {
 function setup() {
 	sessionOutput = createWriter('session01.csv');
 	sessionOutput.write(['Time,Attribute,State\n']);
-	text('Please check and uncheck the sensations listed below as you feel them.');
-	initializeCheckboxes();
-
-	save = createButton('Save session');
-	save.addClass('button');
-	save.mousePressed(closeFile);
+	canvas = createCanvas(windowWidth,windowHeight);
+	noLoop();
+	textSize(20);
+	textAlign(LEFT, 'BASELINE');
+	state = 'INIT';
 }
 
 function initializeCheckboxes() {
@@ -26,7 +34,54 @@ function initializeCheckboxes() {
 	})
 }
 
+function init_view() {
+	init_div = createDiv();
+	init_div.id('init_view');
+	enterName = text("Name:", windowWidth/2 - 160, windowHeight/2-120);
+	name_input = createInput();
+	name_input.input(function(){name = this.value()});
+	name_input.position(windowWidth/2, windowHeight/2 - 148);
+	name_input.parent('init_view');
+
+	enterChemID = text("Chemical ID:", windowWidth/2 - 160, windowHeight/2 - 50);
+	chemID_input = createInput();
+	chemID_input.input(function(){chemID = this.value()});
+	chemID_input.position(windowWidth/2, windowHeight/2 - 80);
+	chemID_input.parent('init_view');
+
+	get_setup_btn = createButton('Get set up!');
+	get_setup_btn.position(windowWidth/2, windowHeight/2 - 60)
+	get_setup_btn.center('horizontal');
+	get_setup_btn.addClass('button btn btn-success');
+	get_setup_btn.parent('init_view');
+	get_setup_btn.mousePressed(click_on_setup);
+}
+
 function draw() { // Update function.
+	background(255,255,255);
+	if(state == 'INIT') {
+		init_view();
+	}
+	if(state == 'ON_SETUP') {
+		text("test", windowWidth/2, windowHeight/2);
+	}
+	if(state == 'ON_TCATA') {
+		text("Please check and uncheck the sensations listed below as you feel them.", windowWidth/2, 40);
+		initializeCheckboxes();
+		save = createButton('Save session');
+		save.addClass('button');
+		save.mousePressed(closeFile);
+	}
+
+}
+
+function click_on_setup() {
+	console.log(name);
+	console.log(chemID);
+	init_div.hide();
+	state = 'ON_SETUP';
+	console.log("ON SET UP");
+	redraw();
 
 }
 
