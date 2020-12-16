5fe7c8ab701c36c855534e5f95ca56cdac06937f
Jasmine Lu <xjasminelu@gmail.com>
2020-12-16 10:05:16 -0600

    node.js app.js included

---
+++ app.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+let server = http.createServer(handleRequest);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+function handleRequest(req, res) {
+  // What did we request?
+  let pathname = req.url;
+
+  // If blank let's ask for index.html
+  if (pathname == '/') {
+    pathname = '/index.html';
+  }
+
+  // Ok what's our file extension
+  let ext = path.extname(pathname);
+
+  // Map extension to file type
+  const typeExt = {
+    '.html': 'text/html',
+    '.js':   'text/javascript',
+    '.css':  'text/css'
+  };
+
+  // What is it?  Default to plain text
+  let contentType = typeExt[ext] || 'text/plain';
+
+  // Now read and write back the file with the appropriate content type
+  fs.readFile(__dirname + pathname,
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading ' + pathname);
+      }
+      // Dynamically setting content type
+      res.writeHead(200,{ 'Content-Type': contentType });
+      res.end(data);
+    }
+  );
+}
